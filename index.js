6e8817339acdb8b79c06a3a276d2cf9e0cc5945e
Nikolai Mavrenkov <koluch@koluch.ru>
2021-01-15 13:23:19 +0300

    Use prettier on commit; add keywords

--- index.js
+++ index.js
@@ -1,30 +1,33 @@
-const fs = require('fs');
-const sass = require('sass');
-const util = require('util');
-const tmp = require('tmp');
-const path = require('path');
+const fs = require("fs");
+const sass = require("sass");
+const util = require("util");
+const tmp = require("tmp");
+const path = require("path");
 
 const sassRender = util.promisify(sass.render);
 const createTempDir = util.promisify(tmp.dir);
 const writeFile = util.promisify(fs.writeFile);
 
 module.exports = {
-  name: 'sass',
+  name: "sass",
   setup: function (build) {
-    build.onResolve({ filter: /.\.(scss|sass)$/, namespace: 'file'}, async (args) => {
-      const fullFileName = path.resolve(args.resolveDir, args.path);
-      const fileExt = path.extname(fullFileName)
-      const baseFileName = path.basename(fullFileName, fileExt)
+    build.onResolve(
+      { filter: /.\.(scss|sass)$/, namespace: "file" },
+      async (args) => {
+        const fullFileName = path.resolve(args.resolveDir, args.path);
+        const fileExt = path.extname(fullFileName);
+        const baseFileName = path.basename(fullFileName, fileExt);
 
-      const sassBuildResult = await sassRender({file: fullFileName});
+        const sassBuildResult = await sassRender({ file: fullFileName });
 
-      const tmpDirPath = await createTempDir();
-      const tmpFilePath = path.resolve(tmpDirPath, `${baseFileName}.css`);
-      await writeFile(tmpFilePath, sassBuildResult.css);
+        const tmpDirPath = await createTempDir();
+        const tmpFilePath = path.resolve(tmpDirPath, `${baseFileName}.css`);
+        await writeFile(tmpFilePath, sassBuildResult.css);
 
-      return {
-        path: tmpFilePath
-      };
-    })
+        return {
+          path: tmpFilePath,
+        };
+      }
+    );
   },
-}
+};
